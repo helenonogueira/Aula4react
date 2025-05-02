0d260a4466a5926826e21b12b2e8a0e1331b955c
Heleno Nogueira Ferreira Junior <helenonogueira21@gmail.com>
2025-05-02 19:13:09 -0300

    aula4

---
+++ aula04/components/Exercicio1.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Exercicio1 = () => {
+  return (
+    <div>
+        <p>Isso é um teste!</p>
+        <img src="diamantina.jpg" />
+    </div>
+  )
+}
+
+export default Exercicio1
---
+++ aula04/components/Exercicio2.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Exercicio2 = () => {
+  return (
+    <div style={{backgroundColor: '#0000ff', color: '#ff0000', width: '200px', textAlign: 'center'}}>
+        Isso é um teste!
+    </div>
+  )
+}
+
+export default Exercicio2
